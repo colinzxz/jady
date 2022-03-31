b397d6f7d5648909aac46ef96c09b766ec14abab
zhangxuezhang <zhangxz16@asiainfo.com>
2022-03-31 16:00:59 +0800

    feat(cli): insert command `changelog`

---
+++ packages/cli/src/commands/changlog.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import ora from 'ora'
+import conventionalChangelog from 'conventional-changelog'
+import { createWriteStream } from 'fs-extra'
+import { CWD } from '../shared/constant'
+
+interface ChangelogOptions {
+  file?: string
+  releaseCount?: number
+}
+
+export const changelog = ({ releaseCount = 0, file = 'CHANGELOG.md' }: ChangelogOptions) => {
+  const o = ora().start('Generating changelog')
+
+  return new Promise<void>((resolve) => {
+    conventionalChangelog({
+      preset: 'angular',
+      releaseCount,
+    }).pipe(createWriteStream(path.resolve(CWD, file))).on('close', () => {
+      o.succeed('Changelog generated success!')
+      resolve()
+    })
+  })
+}
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
+import { changelog } from './commands/changlog'
 import { lint } from './commands/lint'
 import logger from './shared/logger'
 
@@ -9,7 +10,15 @@ const program = new Command()
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 program.version(`jade-cli ${require('../package.json').version}`).usage('<command> [options]')
 
-program.command('lint').action(lint)
+program.command('lint')
+  .description('eslint code')
+  .action(lint)
+
+program.command('changelog')
+  .option('-rc --releaseCount <releaseCount>', 'release count')
+  .option('-f --file <file>', 'changelog filename')
+  .description('generate changelog')
+  .action(changelog)
 
 program.on('command:*', ([cmd]) => {
   program.outputHelp()
