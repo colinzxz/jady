dc40190dc882d0ac42de89965621fc21c87a1c19
zhangxuezhang <zhangxz16@asiainfo.com>
2022-04-01 10:38:38 +0800

    refactor(cli): refactor command `release`
    - insert git work tree check
    - insert change log generate

--- packages/cli/src/commands/release.ts
+++ packages/cli/src/commands/release.ts
@@ -2,21 +2,25 @@ import { resolve } from 'path'
 import { main as bump } from 'bumpp/dist/cli'
 import execa from 'execa'
 import fg from 'fast-glob'
-import { readJsonSync, writeJsonSync } from 'fs-extra'
+import { readJson, writeJson } from 'fs-extra'
 import { CWD } from '../shared/constant'
 import logger from '../shared/logger'
+import { changelog } from './changlog'
 
 export const release = async () => {
   try {
-    const oldVersion = readJsonSync(resolve(CWD, 'package.json')).version
+    await checkGitEmpty()
+
+    const { version: oldVersion } = await readJson(resolve(CWD, 'package.json'))
     await bump([])
-    const version = readJsonSync(resolve(CWD, 'package.json')).version
+    const { version } = await readJson(resolve(CWD, 'package.json'))
 
     if (oldVersion === version) {
-      logger.warning('Release version canceled!')
-      process.exit(1)
+      logger.warning('Release version canceled.')
+      process.exit()
     }
 
+    await changelog()
     await updateVersion(version)
     await gitStore(version)
 
@@ -30,12 +34,12 @@ export const release = async () => {
 async function updateVersion(version: string) {
   const packages = await fg('packages/*/package.json', { cwd: CWD })
 
-  packages.forEach((pkg) => {
+  for (const pkg of packages) {
     const pkgPath = resolve(CWD, pkg)
-    const pkgJson = readJsonSync(pkgPath)
+    const pkgJson = await readJson(pkgPath)
     pkgJson.version = version
-    writeJsonSync(pkgPath, pkgJson, { spaces: 2 })
-  })
+    await writeJson(pkgPath, pkgJson, { spaces: 2 })
+  }
 }
 
 async function gitStore(version: string) {
@@ -44,6 +48,15 @@ async function gitStore(version: string) {
     await execa('git', ['commit', '-m', `v${version}`])
     await execa('git', ['tag', `v${version}`])
   } catch {
-    logger.error('Store package.json has failed, please store manually')
+    logger.error('Store package.json has failed, please store manually.')
+  }
+}
+
+async function checkGitEmpty() {
+  const { stdout } = await execa('git', ['status', '--porcelain'])
+  if (stdout) {
+    logger.error('Git worktree is not empty, please commit changed.')
+
+    process.exit()
   }
 }
